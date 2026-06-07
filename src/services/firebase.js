6a08186552a5ba403bd8937770fbaee46630a12d
Gokul <gokulcynogen12345@gmail.com>
2026-06-07 20:28:48 +0530

    fix: hardcode firebase config for client side bundle

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBjQpWnuac34y_b9UmeC2G_L0kmlN-jYA4",
+    authDomain: "hashtag-saloon.firebaseapp.com",
+    projectId: "hashtag-saloon",
+    storageBucket: "hashtag-saloon.firebasestorage.app",
+    messagingSenderId: "1044580967073",
+    appId: "1:1044580967073:web:0962907be5348ebdc786e0",
+    measurementId: "G-BL3R5QB6Q3"
 };
 
 // Initialize Firebase
